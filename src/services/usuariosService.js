3532c92c745c2cbeda4bf0306ef898e24bc1167f
victorlozada34 <victormanuelcerqueralozada@gmail.com>
2026-06-10 15:34:38 -0500

    agrego servicios users, areas, locations y programs services

---
+++ src/services/usuariosService.js
@@ -0,0 +1,28 @@
+import api from './api'
+
+export const usuariosService = {
+  getAll: async () => {
+    const response = await api.get('/api/usuarios')
+    return response.data
+  },
+
+  getById: async (id) => {
+    const response = await api.get(`/api/usuarios/${id}`)
+    return response.data
+  },
+
+  crear: async (data) => {
+    const response = await api.post('/api/usuarios', data)
+    return response.data
+  },
+
+  actualizar: async (id, data) => {
+    const response = await api.patch(`/api/usuarios/${id}`, data)
+    return response.data
+  },
+
+  eliminar: async (id) => {
+    const response = await api.delete(`/api/usuarios/${id}`)
+    return response.data
+  },
+}
